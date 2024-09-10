162bb8fc0962b976168631720fd727f69e304c92
AriaFantom <priyanshudebnath348@gmail.com>
2024-09-10 23:35:32 +0530

    Initial commit

---
+++ backend/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	s := "gopher"
+	fmt.Printf("Hello and welcome, %s!\n", s)
+
+	for i := 1; i <= 5; i++ {
+
+		fmt.Println("i =", 100/i)
+	}
+}
