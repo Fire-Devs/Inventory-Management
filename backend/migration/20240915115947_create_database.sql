116aeda797606a51b837e5cd68523242b0f7d565
AriaFantom <priyanshudebnath348@gmail.com>
2024-09-16 23:54:41 +0530

    Added Migration Files cuz why not

---
+++ backend/migration/20240915115947_create_database.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    isVerified BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+-- +goose Down
+DROP TABLE users;
